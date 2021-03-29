019eab402b6ef7d5c74ea6c65937609c643a7c6e
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-03-29 23:15:28 +0200

    Add basic realtime volume status

--- webserver.py
+++ webserver.py
@@ -9,13 +9,21 @@ iconManager = IconManager()
 audioManager = AudioManager()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, logger=True, engineio_logger=True)
+socketio = SocketIO(app, logger=True) # engineio_logger=True
+
+def getProcessVolumes():
+    processVolumes = {process[0]:process[2] for process in audioManager.getAllProcessInfo()}
+    return processVolumes
+
+def getProcessIcons():
+    processIcons = {process[0]:iconManager.getIcon(process[0], process[1]) for process in audioManager.getAllProcessInfo()}
+    return processIcons
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    processVolumes = {process[0]:process[2] for process in audioManager.getAllProcessInfo()}
-    processIcons = {process[0]:iconManager.getIcon(process[0], process[1]) for process in audioManager.getAllProcessInfo()}
+    processVolumes = getProcessVolumes()
+    processIcons = getProcessIcons() 
     return render_template("pymixer.html", processVolumes=processVolumes, processIcons=processIcons)
 
 @socketio.on("connected")
@@ -25,6 +33,15 @@ def connected(data):
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
+
+@socketio.on('update')
+def handle_update(data=None):
+    if data:
+        pass
+        # update volume
+    processVolumes = {process[0]:process[2] for process in audioManager.getAllProcessInfo()}
+    socketio.emit("update", processVolumes)
+
 
 if __name__ == '__main__':
     socketio.run(app, host='127.0.0.1', port=5000, debug=True)
